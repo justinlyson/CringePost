58452f9204957d4c73cfac9c4074d124bb705219
Justin <justinalyson@gmail.com>
2019-03-09 23:58:08 -0500

    Updated Download Code

--- Downloading/download.py
+++ Downloading/download.py
@@ -4,12 +4,16 @@ import PIL
 import unidecode
 
 LISTINGS =["hot","new","top","rising","random","controversial"]
+WORD_LENGTH = 2193
+WORDS_PER_SECOND = 2.81
+VIDEO_LENGTH = 780 #13 Minutes
 
-def raw_reddit_json(subreddit, listing,amount):
+def raw_reddit_json(subreddit, listing,amount=15):
     if listing in LISTINGS:
         url = "https://www.reddit.com/r/"+subreddit+"/"+listing+".json"
         params = (
-            ('count', amount),
+            ('count', 1),
+            ('limit', amount)
         )
         response = requests.get('https://www.reddit.com/r/entitledparents/top.json', params=params, headers = {'User-agent': 'downloadScript'}).json()
         return response
@@ -17,6 +21,9 @@ def raw_reddit_json(subreddit, listing,amount):
         print("Invalid Listing option")
         return False
 
+"""
+#Getter Functions for Reddit. Hand it the raw reddit json at whichever element you want.
+"""
 def get_score(post):
     return post['data']['score']
     
@@ -27,23 +34,24 @@ def get_title(post):
     return post['data']['title']
     
 def pretty_write_json(json,out):
+    json = json.dumps(json, indent=4, sort_keys=True)
     with open(out, 'w') as outfile: 
         outfile.write(json)
 
+def get_word_count(story):
+    return len(story.split())
 
-
-response = raw_reddit_json("entitledparents","top","3")
-
-for i in response['data']['children']:
-    print(i['data']['score'])
-
-"""
-response = json.dumps(response, indent=4, sort_keys=True)
-
-
-
-
-with open('post.json', 'w') as outfile: 
-    outfile.write(response)
+def return_stories_amount(subreddit,listing,wordcount=(VIDEO_LENGTH*WORDS_PER_SECOND)): #returns amount of stories
+    response = raw_reddit_json(subreddit,listing)
+    count = 0
+    current_words = 0
+    for i in response['data']['children']:        
+        current_words += get_word_count(get_selftext(i))
+        count += 1
+        if current_words >= wordcount:
+            break
+    return count
     
-"""    
+#MAIN function    
+if __name__ == '__main__':
+    print (return_stories_amount("pettyrevenge","hot"))
